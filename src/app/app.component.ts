586b6dae60dc3280d9cc5d95a977f2d98ade43c9
CheyYong Lim <limcheyyong68777@gmail.com>
2021-04-12 20:35:11 +0800

    angular material dragdrop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { CdkDragDrop, copyArrayItem, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  todo = [
+    "", "", "", "", ""
+  ];
+
+  done = [
+    'Get up',
+    'Brush teeth',
+    'Take a shower',
+    'Check e-mail',
+    'Walk dog'
+  ];
   title = 'drop';
+  drop(event: CdkDragDrop<string[]>) {
+    console.log(event)
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      copyArrayItem(event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
+        this.todo.splice(event.currentIndex+1,1)
+        console.log(event.currentIndex)
+    }
+  }
 }
